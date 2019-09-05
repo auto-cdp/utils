17e9147a8b5e6e42a24d3b8ad50e0e1d4dbff187
liupzmin <676333678@qq.com>
2019-09-05 15:30:33 +0800

    add test

--- afis/afis_test.go
+++ afis/afis_test.go
@@ -1,22 +1,52 @@
 package afis
 
-import "testing"
+import (
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/strslice"
+	"github.com/docker/docker/client"
+	"github.com/docker/go-connections/nat"
+	. "github.com/smartystreets/goconvey/convey"
+	"golang.org/x/net/context"
+	"io"
+	"os"
+	"testing"
+)
 
 func TestDownloadNormal(t *testing.T) {
-	err := DownloadCode("/home/liupeng/test", "http://lp:0124@127.0.0.1/hfp/tst.tar.gz?archive=false")
+
+	cmd := strslice.StrSlice{"--auth-type", "http", "--auth-http", "admin:afis2020", "--upload"}
+	ca := ContainerAttr{}
+	ca.image = "liupzmin/httpfileserver:2.0"
+	ca.cport = "8000"
+	ca.hport = "8000"
+	ca.cmd = cmd
+	ca.cname = "httpserver"
+
+	c, err := createContainer(ca)
+
+	if err != nil{
+		t.Errorf(`createContainer(create container failed: %s) = error`, err.Error())
+	}
+	err = DownloadCode("/tmp", "http://admin:afis2020@127.0.0.1:8000/Dockerfile")
 
 	if err != nil {
-		t.Errorf(`Download("/home/liupeng/test:http://lp:0124@127.0.0.1/hfp/tst.tar.gz?archive=false %s") = error`, err.Error())
+		t.Errorf(`"/tmp", "http://admin:afis2020@127.0.0.1:8000/Dockerfile %s") = error`, err.Error())
+	}
+
+	err = stopContainer(c)
+
+	if err != nil{
+		t.Errorf(`createContainer(stop container failed: %s) = error`, err.Error())
+	}
+
+	err = removeContainer(c)
+
+	if err != nil{
+		t.Errorf(`createContainer(remove container failed: %s) = error`, err.Error())
 	}
 }
 
-func TestDownloadNormalFtp(t *testing.T) {
-	err := DownloadCode("/home/liupeng/test", "ftp://devops:afis2017@192.168.1.9/code/test/deliver_release.py")
-
-	if err != nil {
-		t.Errorf(`Download("/home/liupeng/test:ftp://192.168.1.9/code/test/deliver_release.py %s") = error`, err.Error())
-	}
-}
 
 func TestDownloadNoNormal(t *testing.T) {
 	err := DownloadCode("/root/test", "http://lp:0124@127.0.0.1/hfp/tst.tar.gz")
@@ -30,27 +60,149 @@ func TestDownloadNoNormal(t *testing.T) {
 	}
 }
 
-
-func TestCheckFileOwnerNormal(t *testing.T){
-	if !CheckFileOwner("/home/liupeng/AlarmWechat", "liupeng"){
+func TestCheckFileOwnerNormal(t *testing.T) {
+	if !CheckFileOwner("/home/liupeng/AlarmWechat", "liupeng") {
 		t.Error("/home/liupeng/AlarmWechat:liupeng = error")
 	}
 }
 
-func TestCheckFileOwnerNoNormal(t *testing.T){
-	if CheckFileOwner("/home/liupeng/AlarmWechat", "alarm"){
+func TestCheckFileOwnerNoNormal(t *testing.T) {
+	if CheckFileOwner("/home/liupeng/AlarmWechat", "alarm") {
 		t.Error("/home/liupeng/AlarmWechat:alarm = error")
 	}
 }
 
 func TestIsExecutableNormal(t *testing.T) {
-	if !IsExecutable("/home/liupeng/ant"){
+	if !IsExecutable("/home/liupeng/ant") {
 		t.Error("/home/liupeng/ant = error")
 	}
 }
 
 func TestIsExecutableNoNormal(t *testing.T) {
-	if IsExecutable("/home/liupeng/tcpdump.cap"){
+	if IsExecutable("/home/liupeng/tcpdump.cap") {
 		t.Error("/home/liupeng/tcpdump.cap = error")
 	}
+}
+
+func TestIntegerStuff(t *testing.T) {
+	Convey("Given some integer with a starting value", t, func() {
+		x := 1
+
+		Convey("When the integer is incremented", func() {
+			x++
+
+			Convey("The value should be greater by one", func() {
+				So(x, ShouldEqual, 2)
+			})
+		})
+	})
+}
+
+type ContainerAttr struct {
+	image string
+	cname string
+	cport string
+	hport string
+	cmd   strslice.StrSlice
+}
+
+func createContainer(conattr ContainerAttr) (string, error) {
+
+	cli, err := initDockerClient()
+
+	if err != nil {
+		return "", err
+	}
+
+	ctx := context.Background()
+	imageName := conattr.image
+
+	out, err := cli.ImagePull(ctx, imageName, types.ImagePullOptions{})
+	if err != nil {
+		return "", err
+	}
+
+	_, err = io.Copy(os.Stdout, out)
+
+	if err != nil {
+		return "", err
+	}
+
+	hostBinding := nat.PortBinding{
+		HostIP:   "0.0.0.0",
+		HostPort: conattr.hport,
+	}
+	exports := make(nat.PortSet, 10)
+	containerPort, err := nat.NewPort("tcp", conattr.cport)
+	exports[containerPort] = struct{}{}
+	if err != nil {
+		return "", err
+	}
+
+	portMap := make(nat.PortMap, 0)
+	tmp := make([]nat.PortBinding, 0, 1)
+	tmp = append(tmp, hostBinding)
+	portMap[containerPort] = tmp
+
+	resp, err := cli.ContainerCreate(ctx, &container.Config{
+		Image:        imageName,
+		ExposedPorts: exports,
+		Cmd:          conattr.cmd,
+	},
+		&container.HostConfig{
+			PortBindings: portMap,
+		}, nil, "")
+
+	if err != nil {
+		return "", err
+	}
+
+	if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
+		return "", err
+	}
+
+	return resp.ID, nil
+}
+
+func initDockerClient() (*client.Client, error) {
+
+
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		return nil, err
+	}
+	return cli, nil
+}
+
+
+func stopContainer(containerID string) error{
+	cli, err := initDockerClient()
+
+	if err != nil {
+		return err
+	}
+
+	ctx := context.Background()
+
+	if err = cli.ContainerStop(ctx, containerID, nil); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func removeContainer(containerID string) error{
+	cli, err := initDockerClient()
+
+	if err != nil {
+		return err
+	}
+
+	ctx := context.Background()
+
+	if err = cli.ContainerRemove(ctx, containerID, types.ContainerRemoveOptions{}); err != nil {
+		return err
+	}
+
+	return nil
 }
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/Microsoft/go-winio v0.4.12 // indirect
 	github.com/Nvveen/Gotty v0.0.0-20120604004816-cd527374f1e5 // indirect
 	github.com/cenkalti/backoff v2.2.1+incompatible // indirect
+	github.com/containerd/containerd v1.2.7 // indirect
 	github.com/containerd/continuity v0.0.0-20190426062206-aaeac12a7ffc // indirect
 	github.com/coreos/bbolt v1.3.3 // indirect
 	github.com/coreos/etcd v3.3.13+incompatible
@@ -14,7 +15,9 @@ require (
 	github.com/coreos/go-systemd v0.0.0-20190620071333-e64a0ec8b42a // indirect
 	github.com/coreos/pkg v0.0.0-20180928190104-399ea9e2e55f // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
-	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/docker/distribution v2.7.1+incompatible // indirect
+	github.com/docker/docker v0.0.0-20190813234819-fade624f1696
+	github.com/docker/go-connections v0.4.0
 	github.com/docker/go-units v0.4.0 // indirect
 	github.com/garyburd/redigo v1.6.0
 	github.com/golang/groupcache v0.0.0-20190702054246-869f871628b6 // indirect
@@ -29,12 +32,14 @@ require (
 	github.com/json-iterator/go v1.1.6 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/morikuni/aec v0.0.0-20170113033406-39771216ff4c // indirect
 	github.com/opencontainers/go-digest v1.0.0-rc1 // indirect
 	github.com/opencontainers/image-spec v1.0.1 // indirect
 	github.com/opencontainers/runc v0.1.1 // indirect
 	github.com/ory/dockertest v3.3.4+incompatible // indirect
 	github.com/pkg/errors v0.8.0
 	github.com/satori/go.uuid v1.2.1-0.20181028125025-b2ce2384e17b
+	github.com/smartystreets/goconvey v0.0.0-20190731233626-505e41936337
 	github.com/soheilhy/cmux v0.1.4 // indirect
 	github.com/tmc/grpc-websocket-proxy v0.0.0-20190109142713-0ad062ec5ee5 // indirect
 	github.com/wantedly/gorm-zap v0.0.0-20171015071652-372d3517a876
@@ -43,6 +48,7 @@ require (
 	go.uber.org/atomic v1.4.0 // indirect
 	go.uber.org/multierr v1.1.0 // indirect
 	go.uber.org/zap v1.10.0
+	golang.org/x/net v0.0.0-20190311183353-d8887717615a
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0
 	gopkg.in/ory-am/dockertest.v3 v3.3.4 // indirect
 	gotest.tools v2.2.0+incompatible // indirect
